f1a6e2762189fc751477f6e0fdaa462bf7f314a6
Matthew King <mattking73@gmail.com>
2016-02-20 23:31:21 -0600

    Removed a few unecessary lines, updated copy scripts, using type/more for window line endings now

--- win_makelinks.bat
+++ win_makelinks.bat
@@ -1,6 +1,5 @@
 @echo off
 for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
-  set dow=%%i
   set month=%%j
   set day=%%k
   set year=%%l
@@ -9,8 +8,6 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
 for /f "tokens=1-4 delims=.: " %%i in ("%time%") do (
  set hour=%%i
  set minute=%%j
- set second=%%k
- set hundredth=%%l
 )
 
 ECHO Creating vim backup and swap folders
@@ -35,19 +32,19 @@ ECHO Backing up existing dot files.
 
 :: Makesure HOME has the backslash at the end
 :: Backup dot files
-xcopy "%HOME%_vimrc" "%backup%" /H /Y
-xcopy "%HOME%_gvimrc" "%backup%" /H /Y
-xcopy "%HOME%.gitconfig" "%backup%" /H /Y
-xcopy "%HOME%.gitignore" "%backup%" /H /Y
+copy "%HOME%_vimrc" "%backup%" /Y
+copy "%HOME%_gvimrc" "%backup%" /Y
+copy "%HOME%.gitconfig" "%backup%" /Y
+copy "%HOME%.gitignore" "%backup%" /Y
 
 ECHO.
 ECHO Moving new dot files
 
 :: Overwrites existing files.
-xcopy "%HOME%.dotfiles\vimrc" "%HOME%_vimrc" /Y /H /R
-xcopy "%HOME%.dotfiles\gvimrc" "%HOME%_gvimrc" /Y /H /R
-xcopy "%HOME%.dotfiles\gitconfig" "%HOME%.gitconfig" /Y /H /R
-xcopy "%HOME%.dotfiles\gitignore" "%HOME%.gitignore" /Y /H /R
+TYPE "%HOME%.dotfiles\vimrc" | MORE /p > "%HOME%_vimrc"
+TYPE "%HOME%.dotfiles\gvimrc" | MORE /p > "%HOME%_gvimrc"
+TYPE "%HOME%.dotfiles\gitconfig" | MORE /p > "%HOME%.gitconfig"
+TYPE "%HOME%.dotfiles\gitignore" | MORE /p > "%HOME%.gitignore"
 
 ECHO.
 ECHO Creating vim backup and swap folders
@@ -56,14 +53,5 @@ ECHO Creating vim backup and swap folders
 if not exist "%HOME%\.vim" mkdir "%HOME%\.vim"
 if not exist "%HOME%\.vim\.backups" mkdir "%HOME%\.vim\.backups"
 if not exist "%HOME%\.vim\.swap" mkdir "%HOME%\.vim\.swap"
-
-ECHO Making dotfiles hidden
-
-:: Hide all of the links and backups
-attrib /L +H "%HOME%\_vimrc"
-attrib /L +H "%HOME%\_gvimrc"
-attrib /L +H "%HOME%\.gitconfig"
-attrib /L +H "%HOME%\.gitignore"
-attrib +H "%HOME%\.vim"
 
 ECHO Complete
